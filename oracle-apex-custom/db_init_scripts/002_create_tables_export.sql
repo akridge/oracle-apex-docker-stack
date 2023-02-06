981086f753fb55e500756781a8de2e950e251062
Michael Akridge <akridge@hawaii.edu>
2023-02-06 13:54:35 -1000

    custom docker added

---
+++ oracle-apex-custom/db_init_scripts/002_create_tables_export.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  DDL for Table HERE
+--------------------------------------------------------
+ALTER SESSION SET CONTAINER=XEPDB1;
+
+#insert DDL for tables here
+  
+commit;
---
+++ oracle-apex-custom/delete_stack.sh
@@ -0,0 +1,4 @@
+docker container rm -f auto-xe-reg
+docker container rm -f auto-ords-reg
+docker volume rm db-vol-reg
+docker volume rm ords-config-reg
